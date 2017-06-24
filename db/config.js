e303ae8f602fa242fa15e8ca5530f89f8eaa52e1
Adrian Sutton <adrian@symphonious.net>
2017-06-25 07:53:11 +1000

    Support overriding database config.

--- db/config.js
+++ db/config.js
@@ -4,9 +4,15 @@ exports.migration_path = './patches/';
 // Which DBMS to use for executing migrations
 exports.dbms = 'mysql';
 
+let userConfig = {};
+try {
+    userConfig = require('./config.js.mine');
+} catch (err) {
+    console.debug("No config.js.mine file, continuing.", err);
+}
 // Configuration for MySQL (username, password, etc.)
-exports.mysql = {
+exports.mysql = Object.assign({
     user: 'root',
     password: '',
     database: 'moolah'
-};
+}, userConfig);
