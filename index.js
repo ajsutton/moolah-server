125abed886b186daa9de389b8f5a908ae1006516
Adrian Sutton <suttona@lmax.com>
2017-06-24 20:53:40 +1000

    Use hapi-router to split routing out of main index.js.

--- index.js
+++ index.js
@@ -5,35 +5,35 @@ const Hoek = require('hoek');
 const server = new Hapi.Server();
 server.connection({port: 3000, host: 'localhost'});
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, reply) => {
-        reply('Hello, world!');
-    },
-});
-
-server.register({
-    register: Good,
-    options: {
-        reporters: {
-            console: [
-                {
-                    module: 'good-squeeze',
-                    name: 'Squeeze',
-                    args: [{
-                        response: '*',
-                        log: '*',
-                    }],
-                },
-                {
-                    module: 'good-console',
-                },
-                'stdout',
-            ],
+server.register([
+    {
+        register: Good,
+        options: {
+            reporters: {
+                console: [
+                    {
+                        module: 'good-squeeze',
+                        name: 'Squeeze',
+                        args: [{
+                            response: '*',
+                            log: '*',
+                        }],
+                    },
+                    {
+                        module: 'good-console',
+                    },
+                    'stdout',
+                ],
+            },
         },
     },
-}, err => {
+    {
+        register: require('hapi-router'),
+        options: {
+            routes: 'src/routes/**/*.js',
+        },
+    },
+], err => {
     Hoek.assert(!err, err);
     server.start(err => {
         Hoek.assert(!err, err);
