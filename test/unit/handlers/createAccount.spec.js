463971470bb6cb44e1307006d3969bbac2cbd999
Adrian Sutton <adrian@symphonious.net>
2017-06-26 20:39:30 +1000

    Stub out database so createAccount test can be a unit test.

--- test/unit/handlers/createAccount.spec.js
+++ test/unit/handlers/createAccount.spec.js
@@ -1,5 +1,7 @@
-const serverFactory = require('../../src/server');
+const serverFactory = require('../../../src/server');
 const assert = require('chai').assert;
+const sinon = require('sinon');
+const accountsDao = require('../../../src/db/accountsDao');
 
 describe('Create Account Handler', function() {
     const options = {
@@ -9,14 +11,17 @@ describe('Create Account Handler', function() {
     let server;
 
     beforeEach(async function() {
+        sinon.stub(accountsDao, "create")
         server = await serverFactory.create();
     });
 
     afterEach(function() {
+        accountsDao.create.restore();
         return server.stop();
     });
 
     it('should create an account with a unique id', async function() {
+        accountsDao.create.resolves(null);
         const response = await makeRequest({name: 'Account 1', type: 'cc', balance: 40000});
         assert.equal(response.statusCode, 201);
     });
