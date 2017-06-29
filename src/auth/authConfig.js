832e4bcab8098371049957074a7426e77fb91363
Adrian Sutton <suttona@lmax.com>
2017-06-29 16:37:55 +1000

    Make default auth config silly but functional so it doesn't break tests.

--- src/auth/authConfig.js
+++ src/auth/authConfig.js
@@ -1,9 +1,14 @@
+const uuidv4 = require('uuid/v4');
 let config;
 try {
     config = require('../../config/googleAuthConfig.json');
 } catch (error) {
-    console.warn("Failed to load config/googleAuthConfig.json. Authentication may not work.");
-    config = {};
+    console.warn('Failed to load config/googleAuthConfig.json. Authentication may not work.');
+    config = {
+        secureToken: uuidv4(),
+        googleClientId: 'invalid',
+        clientSecret: 'invalid',
+    };
 }
 
 module.exports = config;
