880cb95c4602ee43aaea6be0bb64e6ad5a740176
Adrian Sutton <suttona@lmax.com>
2017-07-03 16:06:34 +1000

    Simplify database.js.

--- src/db/database.js
+++ src/db/database.js
@@ -6,32 +6,38 @@ const pool = mysql.createPool(Object.assign({
     dateStrings: true,
 }, config));
 
+function getConnection() {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(connection);
+            }
+        });
+    });
+}
 
+function query(connection, sql, args) {
+    return new Promise((resolve, reject) => {
+        connection.query(sql, args, (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
 
 module.exports = {
-    query(sql, ...args) {
-        return new Promise((resolve, reject) => {
-            pool.getConnection((err, connection) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    const query = (sql, args, nextAction) => connection.query(sql, args, (error, results) => {
-                        if (error) {
-                            connection.release();
-                            reject(error);
-                        } else {
-                            nextAction(results);
-                        }
-                    });
-
-                    query('SET sql_mode = "STRICT_ALL_TABLES";', [], () => {
-                        query(sql, args, (results) => {
-                            connection.release();
-                            resolve(results);
-                        });
-                    });
-                }
-            });
-        });
+    async query(sql, ...args) {
+        const connection = await getConnection();
+        try {
+            await query(connection, 'SET sql_mode = "STRICT_ALL_TABLES";', []);
+            return await query(connection, sql, args);
+        } finally {
+            connection.release();
+        }
     },
 };
