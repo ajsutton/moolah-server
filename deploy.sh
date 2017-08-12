666305def8123ed2353c8318c6273b31bfe0fa19
Adrian Sutton <suttona@lmax.com>
2017-08-12 14:32:17 +1000

    Ensure deploy directory exists.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+ssh moolah@do2.symphonious.net mkdir -p versions/incoming
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ moolah@do2.symphonious.net:versions/incoming
 ssh moolah@do2.symphonious.net ./deployIncoming.sh "${TRAVIS_COMMIT}" "${TRAVIS_BUILD_NUMBER}" "${TRAVIS_BUILD_ID}"
