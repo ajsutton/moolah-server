36fa136acc82388af142277bea2935d7286cc5f7
Adrian Sutton <adrian@symphonious.net>
2017-11-16 21:59:19 +1000

    Upgrade dependencies.

--- test/utils/boomOutput.js
+++ test/utils/boomOutput.js
@@ -4,4 +4,5 @@ function makeBoomOutput(boomMethod) {
     return message => JSON.stringify(Boom[boomMethod](message).output.payload);
 }
 
-Object.keys(Boom).forEach(key => exports[key] = makeBoomOutput(key));
+exports.badRequest = makeBoomOutput('badRequest');
+exports.notFound = makeBoomOutput('notFound');
