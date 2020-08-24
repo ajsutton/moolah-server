37cffb80b9245085d1b4c53289223145a85412b5
Adrian Sutton <adrian@symphonious.net>
2020-08-24 13:05:25 +1000

    Upgrade major versions limiting hapi to 19 instead of 20.

--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ exports.create = async function() {
         require('@hapi/cookie'),
         require('@hapi/bell'),
     ]);
+    server.validator(require('joi'));
 
     // Work around for https://github.com/midnightcodr/hapi-mysql2/pull/1 until it gets merged and a new version published
     server.events.on('stop', () => {
