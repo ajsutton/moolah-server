9ac17efc01245ff4982743d7335f61ef40c3ebbf
Adrian Sutton <adrian@symphonious.net>
2021-01-16 08:14:03 +1000

    Less quotes.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 ssh moolah@do2.symphonious.net mkdir -p versions/incoming
-rsync -r --delete-after --quiet "~/project/" moolah@do2.symphonious.net:versions/incoming
+rsync -r --delete-after --quiet ~/project/ moolah@do2.symphonious.net:versions/incoming
 ssh moolah@do2.symphonious.net ./deployIncoming.sh "${CIRCLE_SHA1}" "${CIRCLE_BUILD_NUM}" "${CIRCLE_BUILD_URL}"
