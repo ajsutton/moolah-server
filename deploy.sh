84a3df8434bfb26f6aaf79c667510e09a1fdbe00
Adrian Sutton <adrian@symphonious.net>
2021-01-16 08:05:54 +1000

    Use circleci env var.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ssh moolah@do2.symphonious.net mkdir -p versions/incoming
-rsync -r --delete-after --quiet "${TRAVIS_BUILD_DIR}/" moolah@do2.symphonious.net:versions/incoming
+rsync -r --delete-after --quiet "${CIRCLE_WORKING_DIRECTORY}/" moolah@do2.symphonious.net:versions/incoming
 ssh moolah@do2.symphonious.net ./deployIncoming.sh "${CIRCLE_SHA1}" "${CIRCLE_BUILD_NUM}" "${CIRCLE_BUILD_URL}"
