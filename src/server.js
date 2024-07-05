e40c19d4c00f22f08af31910f98fff9a6704deaa
Adrian Sutton <adrian@symphonious.net>
2024-07-05 11:36:46 +1000

    Disable logging better.

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ export const create = async function () {
     {
       plugin: pino,
       options: {
-        level: logConfig.console.enabled ? 'info' : 'fatal',
+        enabled: logConfig.console.enabled,
         // Redact Authorization headers, see https://getpino.io/#/docs/redaction
         redact: ['req.headers.authorization', 'req.headers.cookie'],
       },
